02d354f51ae87e87eff9f584577e285be8b66522
Suelen Karbivnychyy <suelenmatosr@gmail.com>
2024-04-26 14:41:52 -0400

    Finished first part of lab. Developed a dinamic and clean code to display all trading cards on the ui nicelly

--- static/js/homepage.jsx
+++ static/js/homepage.jsx
@@ -1,7 +1,14 @@
 'use strict';
 
 function Homepage() {
-  return <div>This component needs to be implemented.</div>;
+  return (
+    <React.Fragment>
+      <p>Welcome to the <i>Trade Cards game</i> </p>
+      <a href='/cards'>Click here to go to the Game.</a>
+      <img src='/static/img/balloonicorn.jpg'></img>
+    </React.Fragment>
+
+  );
 }
 
 ReactDOM.render(<Homepage />, document.querySelector('#app'));
